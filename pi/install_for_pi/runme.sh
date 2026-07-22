53b345662e3bbee27d350810ab74e5a2152555e8
Manne Kihlman <manne@kedu.se>
2026-07-22 18:03:55 +0200

    Correct installation instructions and scripts.

--- pi/install_for_pi/runme.sh
+++ pi/install_for_pi/runme.sh
@@ -4,13 +4,13 @@ cp cfg.txt ~
 
 sudo cp showlog /usr/bin
 sudo cp nolog /usr/bin
-sudo cp libavs.so /usr/lib/libavs_so.0
+sudo cp libavs.so /usr/lib/libavs.so.0
 
 # enable any user to use Avantes USB devices
 sudo cp 99-com.rules /etc/udev/rules.d/99-com.rules
 
 # enable sudo users to use date to set time&date without password
-sudo echo "pi ALL=(root) NOPASSWD: /usr/bin/date" > /etc/sudoers.d/date
+sudo cp 010_date /etc/sudoers.d
 
 # enable ssh login
 sudo systemctl enable ssh
